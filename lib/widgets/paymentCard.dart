9d727a5fbcdecf89332aeb6de67a46e96389dcf2
Posiden <osamasda111@gmail.com>
2022-05-02 13:43:37 +0300

    Edit: paymentCard_design

--- lib/widgets/paymentCard.dart
+++ lib/widgets/paymentCard.dart
@@ -14,44 +14,69 @@ class PaymentCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height / 4,
       margin: EdgeInsets.all(10),
-      padding: EdgeInsets.only(bottom: 5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(10)),
         color: Colors.pink[400],
       ),
-      child: Column(
+      child: Row(
+        textDirection: TextDirection.rtl,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+          Column(
             children: [
-              Material(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(9),
-                  topLeft: Radius.circular(0),
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(5),
+              Container(
+                height: MediaQuery.of(context).size.height / 4,
+                margin: EdgeInsets.only(left: 10),
+                decoration: BoxDecoration(
+                  color: Colors.pink[700],
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(9),
+                    bottomRight: Radius.circular(9),
+                  ),
                 ),
-                color: Colors.pink[700],
-                child: IconButton(
-                  color: Colors.white,
-                  onPressed: onPressed,
-                  icon: Icon(Icons.change_circle_sharp),
-                  splashRadius: 20,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      color: Colors.white,
+                      onPressed: onPressed,
+                      icon: Icon(Icons.change_circle_sharp),
+                      splashRadius: 20,
+                    ),
+                  ],
                 ),
-              )
+              ),
             ],
           ),
-          Text("${payment.date} :تاريخ الدفعة"),
-          Text("${payment.payment} :القيمة"),
-          if (payment.description.isNotEmpty)
-            Text("${payment.description} :الوصف"),
-          Align(
-            child: Text("alignment!!"),
-            alignment: AlignmentGeometry.lerp(
-                    Alignment.bottomLeft, Alignment.bottomRight, 0.9)
-                as AlignmentGeometry,
-          )
+          Container(
+            height: MediaQuery.of(context).size.height / 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "تاريخ الدفعة: ${payment.date}",
+                  textDirection: TextDirection.rtl,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "القيمة: ${payment.payment} ",
+                  textDirection: TextDirection.rtl,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                if (payment.description.isNotEmpty)
+                  Text(
+                    "الوصف: ${payment.description} ",
+                    textDirection: TextDirection.rtl,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+              ],
+            ),
+          ),
         ],
       ),
     );
